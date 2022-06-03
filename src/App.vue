5db540a66d99693bb684fb342b456fa113334be3
nestor <nestor@xmlteam.com>
2022-06-02 19:27:51 -0500

    Create project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <music-player/>
+</template>
+
+<script>
+import MusicPlayer from './components/MusicPlayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'music-player': MusicPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="player">
+    <div class="cover">
+      <img src="../assets/vinyl.png" alt="logo"/>
+    </div>
+    <div class="info">
+      <div class="title">
+        Armee der tristen 
+      </div>
+      <div class="singer">
+        Rammstein
+      </div>
+    </div>
+    <div class="volume-box">
+      <span class="volume-down">
+        <i class="material-icons">remove</i>
+      </span>
+      <input type="range" class="volume-range" step="1" value="0" max="100">
+      <span class="volume-up">
+        <i class="material-icons">add</i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicPlayer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
